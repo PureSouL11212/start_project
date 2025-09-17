0b7dec8dd87827c409f89f4c33194b30a44fbf65
ITS_A_PURE_SOUL <sameerkhan4321087@gmail.com>
2025-09-17 12:18:27 +0530

    done

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
 export class RolesComponent {
+firstname:string = "John";
+lastname:string = "Doe";
+age:number = 20;
+isStudent:boolean = true;
+isEmployee:boolean = false;
+isAdmin:boolean = false;
+isSuperAdmin:boolean = false;
+isUser:boolean = false;
+selectstate:string = '';
 
+
+
+showelcomeAlert(){
+  alert("Welcome to Angular");
+}
+
+showmessage(message:string){
+alert(message);
+}
 }
